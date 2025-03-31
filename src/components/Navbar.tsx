60b4df0e693bcc5e8a9557a9a29aff4a37114230
Eduardo ML <edu@eml.run>
2025-03-30 18:51:12 -0600

    fixing navbar v1.1.0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { FiMenu, FiX, FiFacebook, FiInstagram } from "react-icons/fi";
-import { FaWhatsapp } from "react-icons/fa";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
 import styles from "@/styles/Navbar.module.css";
 
 const Navbar = () => {
@@ -15,18 +15,35 @@ const Navbar = () => {
         <div className={styles.left}>
           <div className={styles.socialIcons}>
             <a
-              href="https://www.facebook.com/profile.php?id=61574953281087"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiFacebook />
-            </a>
-            <a
               href="https://www.instagram.com/minureposteria/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FiInstagram />
+              <svg width="0" height="0">
+                <linearGradient
+                  id="instagram-gradient"
+                  x1="0%"
+                  y1="0%"
+                  x2="100%"
+                  y2="100%"
+                >
+                  <stop stopColor="#fccc63" offset="0%" />
+                  <stop stopColor="#f77737" offset="25%" />
+                  <stop stopColor="#e4405f" offset="50%" />
+                  <stop stopColor="#c13584" offset="75%" />
+                  <stop stopColor="#833ab4" offset="90%" />
+                  <stop stopColor="#5b51d8" offset="100%" />
+                </linearGradient>
+              </svg>
+
+              <FaInstagram className={styles.instagramIcon} />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61574953281087"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook className={styles.facebookIcon} />
             </a>
           </div>
         </div>
@@ -38,42 +55,77 @@ const Navbar = () => {
         </div>
 
         <div className={styles.right}>
-          <div className={styles.whatsappButton}>
-            <a
-              href="https://wa.me/1234567890"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaWhatsapp />
-            </a>
-          </div>
-          <button className={styles.menuToggle} onClick={toggleMenu}>
-            {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
-          </button>
+          <a
+            href="https://wa.me/5214771913370"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.whatsappButton}
+          >
+            <FaWhatsapp className={styles.whatsappIcon} />
+            Haz tu pedido
+          </a>
         </div>
+
+        <button className={styles.menuToggle} onClick={toggleMenu}>
+          {menuOpen ? (
+            <FiX size={24} color="#333" />
+          ) : (
+            <FiMenu size={24} color="#333" />
+          )}
+        </button>
+
+        {menuOpen && (
+          <div className={styles.mobileMenu}>
+            <div className={styles.mobileSocialIcons}>
+              <a
+                href="https://instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook />
+              </a>
+            </div>
+            <div className={styles.mobileWhatsapp}>
+              <a
+                href="https://wa.me/5214771913370"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp />
+              </a>
+            </div>
+          </div>
+        )}
       </nav>
 
       {menuOpen && (
         <div className={styles.mobileMenu}>
           <div className={styles.mobileSocialIcons}>
             <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiFacebook />
-            </a>
-            <a
               href="https://instagram.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FiInstagram />
+              <FaInstagram />
+            </a>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
             </a>
           </div>
           <div className={styles.mobileWhatsapp}>
             <a
-              href="https://wa.me/1234567890"
+              href="https://wa.me/5214771913370"
               target="_blank"
               rel="noopener noreferrer"
             >
