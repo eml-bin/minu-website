71436e79bf152b2099c91dee65c0d7041c6628a0
Eduardo ML <edu@eml.run>
2025-04-07 16:51:45 -0600

    saving progress CSS

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -3,28 +3,28 @@ import productImg from "@/assets/product.png";
 export const products = [
   {
     id: 1,
-    name: "Brownie de Chocolate",
+    name: "Macarons de Colores",
     category: "macarons",
     price: 50,
     image: productImg.src,
   },
   {
     id: 2,
-    name: "Cheesecake de Fresa",
+    name: "Pastel de Nata",
     category: "pasteles",
     price: 70,
     image: productImg.src,
   },
   {
-    id: 2,
-    name: "Cheesecake de Fresa",
+    id: 3,
+    name: "Tarta de Nuez",
     category: "tartas",
     price: 70,
     image: productImg.src,
   },
   {
     id: 3,
-    name: "Galletas de Chispas",
+    name: "Galletas M&Ms",
     category: "otros",
     price: 30,
     image: productImg.src,
